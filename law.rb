f6757724a9bb7b602112186d219565fdff6a0763
Eli Parra <eli@elzr.com>
2016-02-27 23:42:59 -0600

    now using nokogiri & redcarpet to generate law.html

---
+++ law.rb
@@ -0,0 +1,21 @@
+require 'nokogiri'
+require 'redcarpet'
+
+law = Nokogiri::XML(File.open("law.xml"))
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
+
+header = File.open("header.html").read
+
+articulos = ''
+law.css("articulo").each do |a|
+	text = a.text.gsub(/^\t+/,'')
+	articulos += '<div class="articulo"><span class="nombre">'+
+			"Artículo #{a.attributes['numero']}."+
+		'</span>'+
+		markdown.render( text )+
+		'</div>'
+end
+
+File.open("law.html", "w+") do |f|
+	f.puts header + articulos + '</body></html>'
+end
